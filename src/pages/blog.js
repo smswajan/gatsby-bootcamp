a0416a421339e1c5a5913f6e8669adeba3d11ad8
smswajan <swajan.mis13.du@gmail.com>
2020-07-11 23:24:26 +0600

    Markdown remark set for blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,40 @@
 import React from 'react'
 import Layout from '../components/layout'
+import { useStaticQuery, graphql } from 'gatsby'
 
 const BlogPage = () => {
+    const data = useStaticQuery(graphql`
+        query{
+            allMarkdownRemark{
+                edges{
+                    node{
+                        frontmatter{
+                            title
+                            date
+                        }
+                        timeToRead
+                    }
+                }
+            }
+        }
+    `)
     return (
         <div>
             <Layout>
                 <h1>Blog</h1>
-                <p>Blog posts will appear here.</p>
+                <ol>
+                    {
+                        data.allMarkdownRemark.edges.map((edge) => {
+                            return (
+                                <li>
+                                    <h2>{edge.node.frontmatter.title} </h2>
+                                    <p>{edge.node.frontmatter.date}</p>
+                                    <p>{edge.node.timeToRead} min read</p>
+                                </li>
+                            )
+                        })
+                    }
+                </ol>
             </Layout>
         </div>
     )
